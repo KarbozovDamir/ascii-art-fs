8069eac90db6eb7ef0a5f13cc28e12196ebddb6a
damirkap89 <damirkap89@gmail.com>
2021-09-20 15:38:57 +0600

    updated code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -13,38 +12,47 @@ const (
 	fileLen = 855
 )
 
+// check amount of arguments
 func main() {
-	if len(os.Args) < 1 { //если нет аргументов выход
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Println("Non-valid amount of arguments")
 		return
 	}
-	font := "standard"               //базовый шрифт
-	switch os.Args[len(os.Args)-1] { //последний элемент в массиве, font
-	case "shadow":
-		font = "shadow"
-	case "thinkertoy":
-		font = "thinkertoy"
-	}
-
-	//отсекаем font, если есть
-	pool := os.Args[1:]                  //скипаем путь
-	if os.Args[len(os.Args)-1] == font { // если последний элемент это font, тогда
-		pool = pool[:len(pool)-1] //исключаем font из списка строк
-	}
-
-	pool, err := makepool(pool)
-	if err != nil {
+	args := os.Args[1:]
+	if !isValid(args[0]) {
 		fmt.Println("Non-valid characters")
 		return
 	}
 
+	text := args[0]    // "hello" == [0]
+	font := "standard" //base font
+	if len(args) == 2 {
+		switch args[1] {
+		case "shadow":
+			font = "shadow"
+		case "thinkertoy":
+			font = "thinkertoy"
+		case "standard":
+			font = "standard"
+		default:
+			fmt.Println("Non-valid font")
+			return
+		}
+	}
+
+	// Read the content of the file
+	argsArr := strings.Split(strings.ReplaceAll(text, "\\n", "\n"), "\n")
+	arr := []string{}
 	readFile, err := os.Open("fonts/" + font + ".txt")
+	defer readFile.Close()
+
 	if err != nil {
 		log.Fatalf("failed to open file: %s", err)
 	}
-	defer readFile.Close()
+
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
-	arr := []string{}
+
 	for fileScanner.Scan() {
 		arr = append(arr, fileScanner.Text())
 	}
@@ -53,21 +61,16 @@ func main() {
 		fmt.Println("File is corrupted")
 		return
 	}
-	printBanners(pool, arr)
-}
-
-func makepool(mass []string) (result []string, err error) {
-	for _, v := range mass {
-		if !isValid(v) {
-			return nil, errors.New("Error")
+	larg := len(argsArr)
+	if larg >= 2 {
+		if argsArr[larg-1] == "" && argsArr[larg-2] != "" {
+			argsArr = argsArr[:larg-1]
 		}
-		mass := strings.Split(strings.ReplaceAll(v, "\\n", "\n"), "\n")
-		result = append(result, mass...) // mass... - массив mass с N-количеством эл-тов
 	}
-	return
+	printBanners(argsArr, arr)
 }
 
-// check for valid of characters by runes from 32 to 126
+// Check for valid of characters by runes from 32 to 126
 func isValid(s string) bool {
 	for _, ch := range s {
 		if ch < ' ' && ch != 10 || ch > '~' {
@@ -77,10 +80,11 @@ func isValid(s string) bool {
 	return true
 }
 
-//print the full outcome
+// Print the full outcome
 func printBanners(banners, arr []string) {
 	for _, ch := range banners {
 		if ch == "" {
+			fmt.Println()
 			continue
 		}
 		for i := 0; i < 8; i++ {
